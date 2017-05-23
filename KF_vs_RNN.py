e8c4010f5628c32151470f49697d7c431e981c6e
Mohammad Aghagolzadeh <mohammad.aghagolzadeh@here.com>
2017-05-23 11:32:37 -0500

    option for kf order

--- KF_vs_RNN.py
+++ KF_vs_RNN.py
@@ -6,16 +6,11 @@ warnings.filterwarnings("ignore")
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 curve_order = 3
-state_order = 2
-
-time_start = -20
-time_end = 20
-time_step = 1
-
-observation_step = 10
+kalman_order = 2
+update_step = 10
 
 #generate sequence
-x = np.arange(time_start,time_end,time_step).reshape(-1,1)
+x = np.arange(-10,20,1).reshape(-1,1)
 y = np.hstack([x**p for p in range(curve_order+1)]).dot(np.random.randn(curve_order+1,1))
 
 #track using KF
@@ -36,7 +31,7 @@ plt.plot(kf.x[0],kf.x[1],'s',color='red',mfc='none');plt.pause(.1)
 statelog = [kf.x.T]
 for i in range(1,x.shape[0]):
   kf.predict()
-  if i<3 or not i%observation_step:
+  if i<3 or not i%update_step:
     kf.update(np.array([x[i],y[i]]).reshape(2,1))
     plt.plot(kf.x[0],kf.x[1],'s',color='blue',mfc='none');plt.pause(.1)
   else:
