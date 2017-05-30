9777c65b89e271f62012170b9ea228be4efe10b9
Mohammad Aghagolzadeh <mohammad.aghagolzadeh@here.com>
2017-05-30 17:52:06 -0500

    add naive (linear) estimates to RNN input

--- KF_vs_RNN.py
+++ KF_vs_RNN.py
@@ -7,19 +7,21 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 from filterpy.kalman import KalmanFilter
 import tensorflow as tf
 ###################################### data parameters
-CURVE_ORDER = 2
+CURVE_ORDER = 3
 CURVE_STEP = .5
 SEQ_LENGTH = 30
-HIST_DEPTH = 2
+POINT_DIM = 5
+OBS_DEPTH = 1
 BATCH_SIZE = 100
-###################################### overhead work
+###################################### load the TF session
+MODEL_SUFFIX = '.tmp'
 print('Loading a saved TF session')
-saver = tf.train.import_meta_graph('rnntracker-polyn%d.meta'%(CURVE_ORDER))
+saver = tf.train.import_meta_graph('rnntracker-polyn%d%s.meta'%(CURVE_ORDER,MODEL_SUFFIX))
 session = tf.Session()
-saver.restore(session,'rnntracker-polyn%d'%(CURVE_ORDER))
+saver.restore(session,'rnntracker-polyn%d%s'%(CURVE_ORDER,MODEL_SUFFIX))
 print('...done')
 ###################################### helper functions
-def gen_data_batch(seq_length,batch_size,curve_order,curve_step,hist_depth):
+def gen_data_batch_backup(seq_length,batch_size,curve_order,curve_step,hist_depth):
   x = np.empty((seq_length,batch_size,3*hist_depth))
   for sample_number in range(batch_size):
     xloc = np.sort(np.random.rand(seq_length)-.5).reshape(seq_length,1)*seq_length/2*curve_step
@@ -31,6 +33,23 @@ def gen_data_batch(seq_length,batch_size,curve_order,curve_step,hist_depth):
     x[:,:,3*level_down:3*(level_down+1)] = np.roll(x[:,:,:3],-1*level_down,axis=0)
   y = np.roll(x[:,:,:2],-hist_depth,axis=0) #future locations
   return x[:-1*hist_depth,:,:], y[:-1*hist_depth,:,:]
+def gen_data_batch(seq_length,batch_size,curve_order,curve_step,obs_depth,point_dim):
+  x = np.zeros((seq_length,batch_size,point_dim*obs_depth))
+  for sample_number in range(batch_size):
+    xloc = np.sort(np.random.rand(seq_length)-.5).reshape(seq_length,1)*seq_length*curve_step
+    yloc = np.hstack([xloc**p for p in range(curve_order+1)]).dot(np.random.randn(curve_order+1,1))
+    x[:,sample_number,0] = xloc.squeeze()
+    x[:,sample_number,1] = yloc.squeeze()
+  dt_next = np.sqrt(((np.roll(x[:,:,:2],-1,axis=0)-x[:,:,:2])**2).sum(axis=2))
+  x[:,:,point_dim-1] = dt_next
+  if point_dim==5:
+    dt_prev = np.sqrt(((np.roll(x[:,:,:2],+1,axis=0)-x[:,:,:2])**2).sum(axis=2))
+    x[:,:,2] = (np.roll(x[:,:,0],+1,axis=0)-x[:,:,0]) /dt_prev *dt_next #linear (naive) estimation using previous observation
+    x[:,:,3] = (np.roll(x[:,:,1],+1,axis=0)-x[:,:,1]) /dt_prev *dt_next #linear (naive) estimation using previous observation
+  for step_back in range(1,obs_depth):
+    x[:,:,point_dim*step_back:point_dim*(step_back+1)] = np.roll(x[:,:,:point_dim],-step_back,axis=0)
+  y = np.roll(x[:,:,:2],-obs_depth,axis=0) #future locations
+  return x[1:-obs_depth,:,:], y[1:-obs_depth,:,:]
 def kf_tracker(kf_instance,seq):
   kf_instance.H = np.array([[1,0,0,0],[0,1,0,0]])
   kf_instance.Q *= 1
@@ -51,26 +70,26 @@ def kf_tracker(kf_instance,seq):
   statelog.append(kf_instance.x.T)
   return np.vstack(statelog)
 ###################################### generate data
-valid_x, valid_y = gen_data_batch(SEQ_LENGTH,BATCH_SIZE,CURVE_ORDER,CURVE_STEP,HIST_DEPTH)
+valid_x, valid_y = gen_data_batch(SEQ_LENGTH,BATCH_SIZE,CURVE_ORDER,CURVE_STEP,OBS_DEPTH,POINT_DIM)
 valid_y_rnn = session.run("prediction:0",{"inputs:0":valid_x})
 fig,ax = plt.subplots(1,3,figsize=(17,9))
 color = np.random.rand(100,3)
 for sample_idx in range(BATCH_SIZE):
   for axi in ax: axi.cla()
   for axi in ax[1:]:
-    axi.plot(valid_y[:-HIST_DEPTH+1,sample_idx,0],valid_y[:-HIST_DEPTH+1,sample_idx,1],'.',color='black')
-    for ll in range(HIST_DEPTH): axi.plot(valid_x[0,sample_idx,3*ll],valid_x[0,sample_idx,3*ll+1],'.',color='blue')
-  for i,point in enumerate(valid_x[:,sample_idx,:3].squeeze()):
+    axi.plot(valid_y[:1-max(OBS_DEPTH,2),sample_idx,0],valid_y[:1-max(OBS_DEPTH,2),sample_idx,1],'k.')
+    for ll in range(OBS_DEPTH): axi.plot(valid_x[0,sample_idx,POINT_DIM*ll],valid_x[0,sample_idx,POINT_DIM*ll+1],'b.')
+  for i,point in enumerate(valid_x[:,sample_idx,:].squeeze()):
     ax[0].plot(point[0],point[1],'.',color=color[i])
-    ax[0].add_patch(plt.Circle((point[0],point[1]),radius=point[2],fill=False,color=color[i]))
+    ax[0].add_patch(plt.Circle((point[0],point[1]),radius=point[POINT_DIM-1],fill=False,color=color[i]))
   ###################################### track using KF
-  statelog = kf_tracker(KalmanFilter(dim_x=4,dim_z=2),valid_x[:,sample_idx,:3].reshape(valid_x.shape[0],3))
+  statelog = kf_tracker(KalmanFilter(dim_x=4,dim_z=2),valid_x[:,sample_idx,[0,1,POINT_DIM-1]].reshape(valid_x.shape[0],3))
   ax[1].plot(statelog[:,0],statelog[:,1],'o',color='red',mfc='none')
   ax[1].plot(statelog[:,0],statelog[:,1],color='green')
   ax[1].set_title('KF')
   ###################################### track using RNN
-  ax[2].plot(valid_y_rnn[:-HIST_DEPTH+1,sample_idx,0],valid_y_rnn[:-HIST_DEPTH+1,sample_idx,1],'o',color='red',mfc='None')
-  ax[2].plot(valid_y_rnn[:-HIST_DEPTH+1,sample_idx,0],valid_y_rnn[:-HIST_DEPTH+1,sample_idx,1],color='green')
+  ax[2].plot(valid_y_rnn[:1-max(OBS_DEPTH,2),sample_idx,0],valid_y_rnn[:1-max(OBS_DEPTH,2),sample_idx,1],'ro',mfc='None')
+  ax[2].plot(valid_y_rnn[:1-max(OBS_DEPTH,2),sample_idx,0],valid_y_rnn[:1-max(OBS_DEPTH,2),sample_idx,1],'g')
   ax[2].set_title('RNN')
   ###################################### display for a few seconds
   ax[0].axis('image')
--- train_lstm_tracker_irregular.py
+++ train_lstm_tracker_irregular.py
@@ -6,48 +6,54 @@ map_fn = tf.map_fn
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
-VERBOSE = False
+VERBOSE = True
 ###################################### helper functions
-def gen_data_batch(seq_length,batch_size,curve_order,curve_step,hist_depth):
-  x = np.empty((seq_length,batch_size,3*hist_depth))
+def gen_data_batch(seq_length,batch_size,curve_order,curve_step,obs_depth,point_dim):
+  x = np.zeros((seq_length,batch_size,point_dim*obs_depth))
   for sample_number in range(batch_size):
-    xloc = np.sort(np.random.rand(seq_length)-.5).reshape(seq_length,1)*seq_length/2*curve_step
+    xloc = np.sort(np.random.rand(seq_length)-.5).reshape(seq_length,1)*seq_length*curve_step
     yloc = np.hstack([xloc**p for p in range(curve_order+1)]).dot(np.random.randn(curve_order+1,1))
     x[:,sample_number,0] = xloc.squeeze()
     x[:,sample_number,1] = yloc.squeeze()
-  x[:,:,2] = np.sqrt(((np.roll(x[:,:,:2],-1,axis=0)-x[:,:,:2])**2).sum(axis=2))
-  for level_down in range(1,hist_depth):
-    x[:,:,3*level_down:3*(level_down+1)] = np.roll(x[:,:,:3],-1*level_down,axis=0)
-  y = np.roll(x[:,:,:2],-hist_depth,axis=0) #future locations
-  return x[:-1*hist_depth,:,:], y[:-1*hist_depth,:,:]
+  dt_next = np.sqrt(((np.roll(x[:,:,:2],-1,axis=0)-x[:,:,:2])**2).sum(axis=2))
+  x[:,:,point_dim-1] = dt_next
+  if point_dim==5:
+    dt_prev = np.sqrt(((np.roll(x[:,:,:2],+1,axis=0)-x[:,:,:2])**2).sum(axis=2))
+    x[:,:,2] = (np.roll(x[:,:,0],+1,axis=0)-x[:,:,0]) /dt_prev *dt_next #linear (naive) estimation using previous observation
+    x[:,:,3] = (np.roll(x[:,:,1],+1,axis=0)-x[:,:,1]) /dt_prev *dt_next #linear (naive) estimation using previous observation
+  for step_back in range(1,obs_depth):
+    x[:,:,point_dim*step_back:point_dim*(step_back+1)] = np.roll(x[:,:,:point_dim],-step_back,axis=0)
+  y = np.roll(x[:,:,:2],-obs_depth,axis=0) #future locations
+  return x[1:-obs_depth,:,:], y[1:-obs_depth,:,:]
 ###################################### data parameters (for training and validation)
 CURVE_ORDER = 3
 CURVE_STEP = .5
-SEQ_LENGTH = 60
-HIST_DEPTH = 2
+SEQ_LENGTH = 30
+POINT_DIM = 5 #number of dimensions per point (x,y,t,...)
+OBS_DEPTH = 1 #observation depth (how many points back in time are observed?)
 ###################################### model parameters
 USE_LSTM = True
-INPUT_SIZE = 3*HIST_DEPTH
+INPUT_SIZE = POINT_DIM*OBS_DEPTH
 OUTPUT_SIZE = 2
 RNN_HIDDEN = 25
 ###################################### training parameters
 BATCH_SIZE = 100
 LEARNING_RATE = .001
-N_EPOCHS = 10000
+N_EPOCHS = 100000
 ITERATIONS_PER_EPOCH = 100
 ###################################### tf graph build-up
 inputs  = tf.placeholder(tf.float32, (None,None,INPUT_SIZE), name='inputs')  # (time, batch, in)
 outputs = tf.placeholder(tf.float32, (None,None,OUTPUT_SIZE)) # (time, batch, out)
 cell = tf.contrib.rnn.BasicLSTMCell(RNN_HIDDEN, state_is_tuple=True)
 initial_state = cell.zero_state(tf.shape(inputs)[1], tf.float32)
-rnn_outputs, rnn_states = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state, time_major=True)
-final_projection = lambda x: layers.fully_connected(x, num_outputs=OUTPUT_SIZE, activation_fn=None)
-predicted_outputs = map_fn(final_projection, rnn_outputs)
-prediction = tf.identity(predicted_outputs, name='prediction') #dummy tensor
-error = tf.reduce_mean((predicted_outputs-outputs)**2)
+rnn_output, rnn_state = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state, time_major=True)
+fc1 = lambda x: layers.fully_connected(x, num_outputs=OUTPUT_SIZE, activation_fn=None)
+fc1_output = map_fn(fc1, rnn_output)
+prediction = tf.identity(fc1_output, name='prediction') #dummy tensor
+error = tf.reduce_mean((fc1_output-outputs)**2)
 train_fn = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(error)
 ###################################### training
-valid_x,valid_y = gen_data_batch(SEQ_LENGTH,100,CURVE_ORDER,CURVE_STEP,HIST_DEPTH)
+valid_x,valid_y = gen_data_batch(SEQ_LENGTH,100,CURVE_ORDER,CURVE_STEP,OBS_DEPTH,POINT_DIM)
 saver = tf.train.Saver()
 with tf.Session() as session:
   session.run(tf.global_variables_initializer())
@@ -59,8 +65,8 @@ with tf.Session() as session:
         sample_out = session.run("prediction:0",{"inputs:0":sample_inp})
         sample_lab = valid_y[:,:1,:]
         plt.clf()
-        for level_down in range(HIST_DEPTH):
-          plt.plot(sample_inp[0,0,0+3*level_down],sample_inp[0,0,1+3*level_down],'k.')
+        for step_back in range(OBS_DEPTH):
+          plt.plot(sample_inp[0,0,POINT_DIM*step_back],sample_inp[0,0,1+POINT_DIM*step_back],'b.')
         plt.plot(sample_lab[:-1,0,0],sample_lab[:-1,0,1],'k.')
         plt.plot(sample_out[:-1,0,0],sample_out[:-1,0,1],'ro',mfc='None')
         plt.plot(sample_out[:-1,0,0],sample_out[:-1,0,1],'g')
@@ -68,7 +74,7 @@ with tf.Session() as session:
       ###################################### update the network
       epoch_error = 0
       for _ in range(ITERATIONS_PER_EPOCH):
-        x,y = gen_data_batch(SEQ_LENGTH,BATCH_SIZE,CURVE_ORDER,CURVE_STEP,HIST_DEPTH)
+        x,y = gen_data_batch(SEQ_LENGTH,BATCH_SIZE,CURVE_ORDER,CURVE_STEP,OBS_DEPTH,POINT_DIM)
         epoch_error += session.run([error,train_fn],{inputs:x,outputs:y})[0]
       epoch_error /= ITERATIONS_PER_EPOCH
       valid_error = session.run(error,{inputs:valid_x,outputs:valid_y})
@@ -76,5 +82,5 @@ with tf.Session() as session:
     ###################################### save the trained net
     saver.save(session,'rnntracker-polyn%d'%(CURVE_ORDER))
   except KeyboardInterrupt:
-    print('\nSaving results...')
+    print('\nSaving results in rnntracker-polyn%d.tmp*'%(CURVE_ORDER))
     saver.save(session,'rnntracker-polyn%d.tmp'%(CURVE_ORDER))
